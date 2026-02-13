600faea1f88c5cdc42491a9f904e87610d5b8c9f
Moiz Ansari <moizansari2005@gmail.com>
2026-02-14 01:47:47 +0500

    Added TokenType

---
+++ src/TokenType.java
@@ -0,0 +1,15 @@
+public enum TokenType {
+    KEYWORD,
+    IDENTIFIER,
+    INT_LITERAL,
+    FLOAT_LITERAL,
+    STRING_LITERAL,
+    CHAR_LITERAL,
+    BOOL_LITERAL,
+    OPERATOR,
+    PUNCTUATOR,
+    COMMENT,
+    WHITE_SPACE,
+    ERROR,
+    EOF // End of File
+}
