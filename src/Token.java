bc5107177a58782402eb5bf5b0d66cbf85ae4110
Moiz Ansari <moizansari2005@gmail.com>
2026-02-14 14:17:47 +0500

    Added Token and TokenType Files

---
+++ src/Token.java
@@ -0,0 +1,20 @@
+public class Token {
+    public TokenType type;
+    public String lexeme;
+    public int line;
+    public int column;
+
+    // Constructor
+    public Token(TokenType type, String lexeme, int line, int column) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.line = line;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        // Required format: <KEYWORD, "start", Line: 1, Col: 1>
+        return "<" + type + ", \"" + lexeme + "\", Line: " + line + ", Col: " + column + ">";
+    }
+}
