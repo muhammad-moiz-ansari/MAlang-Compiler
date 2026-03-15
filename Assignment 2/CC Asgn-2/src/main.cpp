ffb9f12245212262c9163eb071fb69e28d3f2142
abdullah-siddiqui31 <abdullah.siddi31@gmail.com>
2026-03-15 17:03:35 +0500

    Indirect Left Recursion Removed

--- Assignment 2/CC Asgn-2/src/main.cpp
+++ Assignment 2/CC Asgn-2/src/main.cpp
@@ -93,6 +93,62 @@ void solveRecursion(vector<Production>& alphaProds, vector<Production>& betaProd
     }
 }
 
+void substitute(GrammarRule& rc, vector<Production>& betaProds, vector<Production>& p, int i) {
+    for (int k = 0; k < rc.prods.size(); k++) {
+        Production pp;
+        for (int l = 0; l < rc.prods[k].symbols.size(); l++) {
+            pp.symbols.push_back(rc.prods[k].symbols[l]);
+        }
+        for (int k = 1; k < betaProds[i].symbols.size();k++) {
+            pp.symbols.push_back(betaProds[i].symbols[k]);
+        }
+        p.push_back(pp);
+    }
+}
+
+void removeExtraTerms(vector<Production>& betaProds, vector<int>& indices) {
+    vector<Production> temp;
+    for (int i = 0; i < betaProds.size(); i++) {
+        bool con = 0;
+        for (int j = 0;j < indices.size();j++) {
+            if (i == indices[j]) {
+                con = 1;
+                break;
+            }
+        }
+        if (!con) {
+            temp.push_back(betaProds[i]);
+        }
+    }
+    while (!betaProds.empty())
+        betaProds.pop_back();
+    for (int i = 0; i < temp.size();i++)
+        betaProds.push_back(temp[i]);
+}
+
+void RemoveIndirectLeftRecursion(Grammar r, vector<Production>& betaProds, string nt) {
+    if (betaProds.empty()) { return; }
+
+    vector<Production> p;
+    vector<int> indices;
+    // A -> B | BC
+    for (int i = 0;i < betaProds.size(); i++) {
+        GrammarRule rc = r.rules[betaProds[i].symbols[0]];   // B -> AC | CD | X
+        if (rc.nonTerminal == nt)   continue;
+        for (int j = 0;j < rc.prods.size(); j++) {
+            if (rc.prods[j].symbols[0] == nt) {
+                substitute(rc, betaProds, p, i);
+                indices.push_back(i);
+                break;
+            }
+        }
+    }
+    removeExtraTerms(betaProds, indices);
+    for (int k = 0; k < p.size(); k++)
+        betaProds.push_back(p[k]);
+
+}
+
 Grammar* eliminateRecursion(Grammar& d) {
     Grammar *g = new Grammar();         // This new object will contain removed left recursive grammar
     g->terminals = d.terminals;
@@ -105,12 +161,16 @@ Grammar* eliminateRecursion(Grammar& d) {
         GrammarRule rc;
         rc.nonTerminal = d.nonTerminals[i];
 
+        RemoveIndirectLeftRecursion(d, r.prods, d.nonTerminals[i]);
+        
         // Separates alpha and beta productions
         vector<Production> alphaProds, betaProds;
         separateAlphaBetaProductions(alphaProds, betaProds, r);
 
+
         // Solves Recursion if exists
         solveRecursion(alphaProds, betaProds, rc, g);
+        d.rules[d.nonTerminals[i]] = g->rules[d.nonTerminals[i]];
     }
     return g;
 }
@@ -121,13 +181,28 @@ int main() {
     cout << "=== Original Grammar ===\n";
     printGrammar(g);
 
-    Grammar* gg = eliminateRecursion(g);
-    printGrammar(*gg);
+    for (int i = 1;i <= 5;i++) {
+        if (i == 1)
+            cout << "A -> Aa | B Test" << endl << endl;
+        if (i == 2)
+            cout << "A -> Xa | B, X -> Ac Test" << endl << endl;
+        if (i == 3)
+            cout << "A -> Aa Test" << endl << endl;
+        if (i == 4)
+            cout << "A -> A | B Test" << endl << endl;
+        if (i == 5)
+            cout << "A -> A Test" << endl << endl;
+
+        Grammar g = loadGrammar("left_Recursion_Test" + to_string(i) + ".txt");
+        Grammar* gg = eliminateRecursion(g);
+        printGrammar(*gg);
+        cout << endl <<"-----------------------------" << endl;
+    }
 
     
     // Left Factoring
     cout << "=== Left Factored Grammar ===\n";
-    leftFactor(*gg);
-    printGrammar(*gg);
+    leftFactor(g);
+    printGrammar(g);
     return 0;
 }
