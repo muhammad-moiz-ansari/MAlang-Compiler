9d118d82eefe0ad65d88648cb01b8f4675f26699
Moiz Ansari <moizansari2005@gmail.com>
2026-04-18 13:27:13 +0500

    A3: Added Grammar files

---
+++ Assignment 3/CC Asgn-3/src/grammar.cpp
@@ -0,0 +1,144 @@
+#include "grammar.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+bool isNonTerminal(const string& symbol) {
+	return !symbol.empty() && isupper(symbol[0]);
+}
+
+bool isTerminal(const string& symbol) {
+	return !symbol.empty() && !isNonTerminal(symbol) && !isEpsilon(symbol);
+}
+
+bool isEpsilon(const string& symbol) {
+	return (symbol == "@" || symbol == "epsilon");
+}
+
+GrammarRule parseLine(const string& line) {
+	GrammarRule rule;
+	stringstream ss(line);
+	string token;
+	
+	ss >> rule.nonTerminal;		// Non-terminal (LHS)
+	ss >> token;				// The "->" symbol
+
+	// Productions (RHS)
+	Production prod;
+	while (ss >> token) {
+		if (token == "|") {
+			rule.prods.push_back(prod);
+			prod = Production();  // start fresh
+		}
+		else
+			prod.symbols.push_back(token);
+	}
+	if (!prod.symbols.empty())
+		rule.prods.push_back(prod);  // push last production
+
+	return rule;
+}
+
+Grammar loadGrammar(const string& filename) {
+	Grammar g;
+	string line;
+	bool isStartSymbol = true;
+	ifstream file(filename);
+	if (!file.is_open()) {
+		cout << "Error: could not open file: " << filename << endl;
+		exit(1);
+	}
+
+	while (getline(file, line)) {
+		if (line.empty())
+			continue;
+
+		GrammarRule rule = parseLine(line);
+		g.rules[rule.nonTerminal] = rule;
+		g.nonTerminals.push_back(rule.nonTerminal);
+
+		if (isStartSymbol) {
+			g.startSymbol = rule.nonTerminal;
+			isStartSymbol = false;
+		}
+	}
+
+	// Collecting all non-terminals
+	for (const auto& nt : g.nonTerminals) {
+		auto& rule = g.rules.at(nt);
+		for (int i = 0; i < rule.prods.size(); ++i) {
+			for (int j = 0; j < rule.prods[i].symbols.size(); ++j) {
+				auto symbol = rule.prods[i].symbols[j];
+				// Add the ternimal to list if doesnt already exists
+				if (isTerminal(symbol)) {
+					if (find(g.terminals.begin(), g.terminals.end(), symbol) == g.terminals.end())
+						g.terminals.push_back(symbol);
+				}
+			}
+		}
+	}
+	return g;
+}
+
+void printGrammar(const Grammar& g) {
+	// For each non-terminal
+	for (const auto& nonT : g.nonTerminals) {
+		const auto& rule = g.rules.at(nonT);
+		cout << nonT << " -> ";
+
+		// For each production of that non-terminal
+		for (int i = 0; i < rule.prods.size(); ++i) {
+			const auto& prod = rule.prods[i];
+
+			// For each symbol of that production
+			for (const auto& symbol : prod.symbols) {
+				cout << symbol << " ";
+			}
+			if (i != rule.prods.size() - 1)
+				cout << "| ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+
+void saveGrammar(const Grammar& g, const string& filename) {
+	// Open the file for writing
+	ofstream outFile(filename);
+	if (!outFile.is_open()) {
+		cout << "Error: Could not open " << filename << " for writing.\n";
+	}
+
+	// For each non-terminal
+	for (const auto& nonT : g.nonTerminals) {
+		const auto& rule = g.rules.at(nonT);
+
+		// Print to console and file
+		if (outFile.is_open()) outFile << nonT << " -> ";
+
+		// For each production of that non-terminal
+		for (int i = 0; i < rule.prods.size(); ++i) {
+			const auto& prod = rule.prods[i];
+
+			// For each symbol of that production
+			for (const auto& symbol : prod.symbols) {
+				if (outFile.is_open()) outFile << symbol << " ";
+			}
+
+			// Handle the pipe separator
+			if (i != rule.prods.size() - 1) {
+				if (outFile.is_open()) outFile << "| ";
+			}
+		}
+		if (outFile.is_open()) outFile << endl;
+	}
+
+	// Close the file and confirm
+	if (outFile.is_open()) {
+		outFile << endl;
+		outFile.close();
+		cout << "\n[Success] Grammar safely saved to " << filename << "\n\n";
+	}
+}
---
+++ Assignment 3/CC Asgn-3/src/grammar.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+// ── Data Structures ──────────────────────────────────────
+
+struct Production {
+    vector<string> symbols;     // All the terminals & non-terminals
+};
+
+struct GrammarRule {
+    string nonTerminal;         // L.H.S of arrow (->)
+    vector<Production> prods;   // R.H.S of arrow (->)
+};
+
+struct Grammar {
+    vector<string> nonTerminals;  // ordered, first = start symbol
+    vector<string> terminals;
+    map<string, GrammarRule> rules;
+    string startSymbol;
+};
+
+// ── Function Declarations ─────────────────────────────────
+
+bool isNonTerminal(const string& symbol);   // uppercase first char?
+bool isTerminal(const string& symbol);      // lowercase first char, digit, or punctuation (except @)
+bool isEpsilon(const string& symbol);       // "epsilon" or "@"
+GrammarRule parseLine(const string& line);
+Grammar loadGrammar(const string& filename);   // reads grammar.txt
+void printGrammar(const Grammar& g);        // for debugging
+void saveGrammar(const Grammar& g, const string& filename = "output/grammar_transormed.txt");
