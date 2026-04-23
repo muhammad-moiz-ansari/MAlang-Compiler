b091b1cf1e9a26b2e3bd21ebf00ee505b3743866
Moiz Ansari <moizansari2005@gmail.com>
2026-04-23 22:36:43 +0500

    A3: Added input files + Added time measurement

--- Assignment 3/CC Asgn-3/src/main.cpp
+++ Assignment 3/CC Asgn-3/src/main.cpp
@@ -4,6 +4,7 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <chrono>
 #include "grammar.h"
 #include "grammar_augment.h"
 #include "first_follow.h"
@@ -12,13 +13,36 @@
 #include "tree.h"
 using namespace std;
 
-void saveComparison(int slrSize, int lr1Size, const string& filename);
-void parse_input_file(const Grammar& g, const string& inputFileType, const string& displayTitle, const string& outFileName, bool isFirstFile);
+void saveComparison(int slrSize, int lr1Size, long long slrTime, long long lr1Time, const string& filename); 
+void parse_input_file(const Grammar& g, const string& inputFileType, const int& langNum, const string& displayTitle, const string& outFileName, bool isFirstFile);
 
 int main() {
+	int langNum;  // Change this to switch between grammar1, grammar2, etc and input_valid1, input_valid2, etc
+	string grammarFileName;
+
+	// ====================== 0. Select input files ======================
+	cout << "Choose one of the following grammars (Corresponding input files will be selected automatically):\n";
+	cout << "1. grammar1.txt\n2. grammar2.txt\n3. grammar3.txt\n4. grammar_with_conflict.txt\n";
+    while (true)
+    {
+        cout << "Enter choice (1-4): ";
+		cin >> langNum;
+        if (langNum >= 1 && langNum <= 4) {
+            break;
+        }
+        else {
+            cout << "Invalid choice! Please enter a number between 1 and 4.\n";
+		}
+    }
+    if (langNum == 4)
+        grammarFileName = "input/grammar_with_conflict.txt";
+    else
+		grammarFileName = "input/grammar" + to_string(langNum) + ".txt";
+
+
     // ====================== 1. Load grammar ======================
     Grammar g;
-    g = loadGrammar("input/grammar1.txt");
+    g = loadGrammar(grammarFileName);
     cout << "=== Original Grammar ===\n";
     printGrammar(g);
 
@@ -39,7 +63,18 @@ int main() {
 
     // ====================== 4. SLR(1) Item Sets + Table ======================
     cout << "\n========== SLR(1) ==========\n";
+
+    // Start clock
+    auto slr_start = chrono::high_resolution_clock::now();
+
     vector<ItemState> slrStates = buildSLR(g, FOLLOW, FIRST);
+
+    // Stop clock
+    auto slr_end = chrono::high_resolution_clock::now();
+
+    // Calculate duration in microseconds
+    auto slr_time = chrono::duration_cast<chrono::microseconds>(slr_end - slr_start).count() / 1000;
+    cout << "Time to construct SLR(1) table: " << slr_time << " milliseconds\n";
 
     cout << "\n--- LR(0) Item Sets ---\n";
     printNsaveItemSets(slrStates, "output/slr_items.txt");
@@ -50,13 +85,24 @@ int main() {
     // ====================== 5. SLR(1) Parse input strings ======================
     cout << "\n--- SLR(1) Parsing Traces ---\n";
     // true = overwrite the file
-    parse_input_file(g, "valid", "SLR(1) - Valid", "output/slr_trace.txt", true);
+    parse_input_file(g, "valid", langNum, "SLR(1) - Valid", "output/slr_trace.txt", true);
     // false = append to the file
-    parse_input_file(g, "invalid", "SLR(1) - Invalid", "output/slr_trace.txt", false);
+    parse_input_file(g, "invalid", langNum, "SLR(1) - Invalid", "output/slr_trace.txt", false);
 
     // ====================== 6. LR(1) Item Sets + Table ======================
     cout << "\n========== LR(1) ==========\n";
+
+    // Start clock
+    auto lr1_start = chrono::high_resolution_clock::now();
+
     vector<ItemState> lr1States = buildLR1(g, FOLLOW, FIRST);
+
+    // Stop clock
+    auto lr1_end = chrono::high_resolution_clock::now();
+
+    // Calculate duration in microseconds
+    auto lr1_time = chrono::duration_cast<chrono::microseconds>(lr1_end - lr1_start).count() / 1000;
+    cout << "Time to construct LR(1) table: " << lr1_time << " milliseconds\n";
 
     cout << "\n--- LR(1) Item Sets ---\n";
     printNsaveItemSets(lr1States, "output/lr1_items.txt");
@@ -67,16 +113,18 @@ int main() {
     // ====================== 7. LR(1) Parse input strings ======================
     cout << "\n--- LR(1) Parsing Traces ---\n";
     // true = overwrite the file
-    parse_input_file(g, "valid", "LR(1) - Valid", "output/lr1_trace.txt", true);
+    parse_input_file(g, "valid", langNum, "LR(1) - Valid", "output/lr1_trace.txt", true);
     // false = append to the file
-    parse_input_file(g, "invalid", "LR(1) - Invalid", "output/lr1_trace.txt", false);
+    parse_input_file(g, "invalid", langNum, "LR(1) - Invalid", "output/lr1_trace.txt", false);
 
     // ====================== 8. Comparison ======================
     cout << "\n========== Comparison ==========\n";
     cout << "SLR(1) states : " << slrStates.size() << "\n";
     cout << "LR(1)  states : " << lr1States.size() << "\n";
     cout << "Extra states in LR(1): " << (lr1States.size() - slrStates.size()) << "\n";
-    saveComparison(slrStates.size(), lr1States.size(), "output/comparison.txt");
+    cout << "SLR(1) Table Construction Time: " << slr_time << " milliseconds\n";
+    cout << "LR(1)  Table Construction Time: " << lr1_time << " milliseconds\n";
+    saveComparison(slrStates.size(), lr1States.size(), slr_time, lr1_time, "output/comparison.txt");
 
     return 0;
 }
@@ -84,9 +132,9 @@ int main() {
 
 /////////////////////// FUNCTION DEFINITIONS ///////////////////////
 
-void parse_input_file(const Grammar& g, const string& inputFileType, const string& displayTitle, const string& outFileName, bool isFirstFile)
+void parse_input_file(const Grammar& g, const string& inputFileType, const int& langNum, const string& displayTitle, const string& outFileName, bool isFirstFile)
 {
-    ifstream fin("input/input_" + inputFileType + ".txt");
+    ifstream fin("input/input_" + inputFileType + to_string(langNum) + ".txt");
     string line;
 
     // True for first parse of this type of parser
@@ -103,7 +151,7 @@ void parse_input_file(const Grammar& g, const string& inputFileType, const strin
     }
 }
 
-void saveComparison(int slrSize, int lr1Size, const string& filename) {
+void saveComparison(int slrSize, int lr1Size, long long slrTime, long long lr1Time, const string& filename) {
     // Open the file for writing
     ofstream outFile(filename);
     if (!outFile.is_open()) {
@@ -114,6 +162,9 @@ void saveComparison(int slrSize, int lr1Size, const string& filename) {
     outFile << "SLR(1) states : " << slrSize << "\n";
     outFile << "LR(1)  states : " << lr1Size << "\n";
     outFile << "Extra states in LR(1): " << (lr1Size - slrSize) << "\n";
+
+    outFile << "SLR(1) Table Construction Time: " << slrTime << " milliseconds\n";
+    outFile << "LR(1)  Table Construction Time: " << lr1Time << " milliseconds\n";
     
     if (outFile.is_open()) {
         outFile << endl;
